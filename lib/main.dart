f31ef4ed94c114a49638e666f47a63031fb535f7
ccsaposs <ccsaposs+github@gmail.com>
2019-03-22 09:57:21 -0700

    Update version number to 1.6

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:qr_flutter/qr_flutter.dart';
 
 part 'firebase.dart';
 
-final version = '1.5';
+final version = '1.6';
 
 // Creates a single, global instance
 final FirebaseDatabase database = FirebaseDatabase.instance;
