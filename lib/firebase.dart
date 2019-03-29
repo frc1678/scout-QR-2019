c751f4c6ebf77efdc8ee279c80e16de676500a85
Justin Yeung <justinmyeung@gmail.com>
2019-03-28 17:31:27 -0700

    Update database for AVR (#8)

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -5,14 +5,14 @@ Future<FirebaseApp> configureDatabase() async {
       name: 'db2',
       options: Platform.isIOS
         ? const FirebaseOptions(
-          googleAppID: '1:590951349495:ios:a1bd959b93903331',
+          googleAppID: '1:959322619782:ios:a1bd959b93903331',
           gcmSenderID: '',
-          databaseURL: 'https://scouting-2019-sac-2bdd7.firebaseio.com',
+          databaseURL: 'https://scouting-2019-avr-cacf9.firebaseio.com',
         )
         : const FirebaseOptions( // Android
-          googleAppID: '1:590951349495:android:a1bd959b93903331',
-          apiKey: 'AIzaSyAHmQsrjNA7ZsKPp6poUdG_HeILOOBzCBg',
-          databaseURL: 'https://scouting-2019-sac-2bdd7.firebaseio.com',
+          googleAppID: '1:959322619782:android:a1bd959b93903331',
+          apiKey: 'AIzaSyCrWVk5t0-2QchRmzA7_LfOXJ2uXronJc0',
+          databaseURL: 'https://scouting-2019-avr-cacf9.firebaseio.com',
         )
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:qr_flutter/qr_flutter.dart';
 
 part 'firebase.dart';
 
-final version = '1.6';
+final version = '1.7';
 
 // Creates a single, global instance
 final FirebaseDatabase database = FirebaseDatabase.instance;
